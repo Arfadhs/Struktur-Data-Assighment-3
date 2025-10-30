61e6338aa388e9b1cbe28b962c056883f4d1962e
Arfadhs <ariffadlilhasibuan@gmail.com>
2025-10-30 17:48:46 +0700

    Add files via upload

---
+++ MODUL 5/Unguided/SLL.cpp
@@ -0,0 +1,56 @@
+#include "SLL.h"
+
+void createList(List& L) {
+    L.first = Nil;
+}
+
+address alokasi(infotype x) {
+    address P = new ElmList;
+    if (P != Nil) {
+        P->info = x;
+        P->next = Nil;
+    }
+    return P;
+}
+
+void dealokasi(address P) {
+    delete P;
+}
+
+void printInfo(List L) {
+    address P = L.first;
+    while (P != Nil) {
+        cout << P->info << " ";
+        P = P->next;
+    }
+    cout << endl;
+}
+
+void insertFirst(List& L, address P) {
+    if (P != Nil) {
+        P->next = L.first;
+        L.first = P;
+    }
+}
+
+address findElm(List L, infotype x) {
+    address P = L.first;
+    while (P != Nil) {
+        if (P->info == x) {
+            return P;
+        }
+        P = P->next;
+    }
+    return Nil;
+}
+
+
+int sumInfo(List L) {
+    int total = 0;
+    address P = L.first;
+    while (P != Nil) {
+        total += P->info;
+        P = P->next;
+    }
+    return total;
+}
